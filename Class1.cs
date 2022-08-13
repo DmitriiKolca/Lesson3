095ef478a0c3b6aa01c24a3fca15eea4e3ac921d
Dmitrii Kolca <leonidi4@list.ru>
2022-08-13 13:18:05 +0300

    ProgrammRetirementAge

---
+++ Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logisticspase
+{
+    public class Person
+    {
+        public static int retirementAge = 65;
+
+        int enteredByUserAge;
+
+        public int ChangeConstretirementAge 
+        {
+            set
+            {
+                if (value < 100 && value > 1)  
+                { 
+                    retirementAge = value;
+                }                    
+                else 
+                {
+                    retirementAge=65;
+                    Console.WriteLine("Неверно введет пенсионный возраст");                    
+                }                  
+            }
+            get
+            {
+                return retirementAge;
+            }
+        }// Свойство замены статической переменной пенсионного возраста
+
+        public Person (int EnteredByUserAge)
+        {
+            enteredByUserAge = EnteredByUserAge;
+            Console.WriteLine($"Конструктор введенный пользователем возраст {enteredByUserAge}");
+        }// конструктор
+
+        public void CheckAge() 
+        {
+            if (enteredByUserAge >= retirementAge)
+                Console.WriteLine("Уже на пенсии");
+            else
+                Console.WriteLine($"До пенсионного возраста еще {retirementAge - enteredByUserAge}");
+
+        } // Функция логики, возраст до пенсии
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Logisticspase;
+
+
+namespace Lesson3
+{
+    public class Work
+    {   
+        public static void Main(string[] arrou)
+        {
+            Person Dima = new Person(30);
+            
+            Dima.ChangeConstretirementAge = 105;  // Изменил через свойство пенсионный возраст
+                                                  // конкретно для Dima
+            
+            Console.WriteLine(Dima.ChangeConstretirementAge);// Это для проверки
+       
+            Dima.CheckAge();
+         
+        }
+       
+    }
+
+
+}
+
